5e49df63f3f8efbabd966c7a288ea8f70f6aa2ff
lakbir <lakbir1993@gmail.com>
2024-04-28 01:47:03 +0200

    Create a new Spring boot project, Add final dependencies, Prepare the infrastructure with Docker Compose, Configure the project, Spring security Overview, Create the user entity, Create the Role entity, Implement user and role repositories, Create the Token entity and repository

---
+++ book-network/src/main/java/com/lakbir/book/entities/Token.java
@@ -0,0 +1,29 @@
+package com.lakbir.book.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
