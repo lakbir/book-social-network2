ed870abf27b5816096e9c5326d0128943d48832f
lakbir <lakbir1993@gmail.com>
2024-05-17 00:16:25 +0200

    Implement the register method start implement the Email sender service

--- book-network/src/main/java/com/lakbir/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/lakbir/book/auth/AuthenticationService.java
@@ -1,9 +1,69 @@
 package com.lakbir.book.auth;
 
+import com.lakbir.book.email.EmailService;
+import com.lakbir.book.entities.Token;
+import com.lakbir.book.entities.User;
+import com.lakbir.book.repositories.RoleRepository;
+import com.lakbir.book.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final EmailService emailService;
+
     public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new IllegalStateException("Role USER was not initialized"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
     }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+    String generateToken = generateActivationCode(6);
+   var token = Token.builder()
+           .token(generateToken)
+           .createAt(LocalDateTime.now())
+           .expiresAt(LocalDateTime.now().plusMinutes(15))
+           .user(user)
+           .build();
+
+    return generateToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for(int i=0; i< length; i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+
+
 }
---
+++ book-network/src/main/java/com/lakbir/book/email/EmailService.java
@@ -0,0 +1,59 @@
+package com.lakbir.book.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.validation.MessageInterpolator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    public void sendEmail(
+            String to,
+            String username,
+            EmailTemplateName emailTemplate,
+            String confirmationUrl,
+            String activationCode,
+            String subject
+    ) throws MessagingException {
+
+        String templateName;
+        if(emailTemplate == null){
+            templateName = "confirm-email";
+        } else {
+            templateName = emailTemplate.name();
+        }
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                mimeMessage,
+                MimeMessageHelper.MULTIPART_MODE_MIXED,
+                StandardCharsets.UTF_8.name()
+        );
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username",username);
+        properties.put("confirmationUrl",confirmationUrl);
+        properties.put("activation_code",activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        helper.setFrom("lakbir.cry@gmail.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+    }
+}
